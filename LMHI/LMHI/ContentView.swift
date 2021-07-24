991f3bef0d83af49219bece2743e4bc58f178001
AlmadelX <almadel@almadelx.ru>
2021-07-24 08:29:29 +0500

    Created XCode project

---
+++ LMHI/LMHI/ContentView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("LMHI")
+            .font(.title)
+            .fontWeight(.bold)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
