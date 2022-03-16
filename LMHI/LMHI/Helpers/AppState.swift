a186dfbf1403f72290d6b91db65bff984bb64fa9
AlmadelX <almadel@almadelx.ru>
2022-03-16 12:44:09 +0500

    Tested and fixed

--- LMHI/LMHI/Helpers/AppState.swift
+++ LMHI/LMHI/Helpers/AppState.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 class AppState {
-    static var isInDebugMode: Bool = true
+    static var isInDebugMode: Bool = false
     static private var storage = UserDefaults.standard
     
     static func debugLog<Data>(_ data: Data) {
